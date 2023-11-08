ba8350dc7f274bf4cb39b49744c90c4329c03a16
Decision <personal@decision01.com>
2023-11-08 17:00:09 +0800

    feat: replace channel by sync.map

--- core/txpool.go
+++ core/txpool.go
@@ -23,26 +23,15 @@ type TxPool struct {
 	waitingQueue chan *common.Transaction
 	txs          sync.Map
 
-	//packing  bool
-	//packCond *sync.Cond
-	//lock     *sync.Mutex
-
 	flags  sync.Map
 	height int
 }
 
 func NewTxPool(chain *BlockChain) *TxPool {
 	txOnce.Do(func() {
-		//lock := &sync.Mutex{}
-
 		txPoolInst = &TxPool{
 			chain:   chain,
-			txQueue: make(chan string, 40960),
-			//packing:  false,
-			//lock:     lock,
-			//packCond: sync.NewCond(lock),
-			//txs:     sync.Map{},
-			//txs: make(map[common.Hash]*common.Transaction),
+			txQueue: make(chan string, 10240),
 		}
 	})
 	return txPoolInst
@@ -52,67 +41,59 @@ func GetTxPoolInst() *TxPool {
 	return txPoolInst
 }
 
-//func (pool *TxPool) setPackStart() {
-//	log.Infoln("Set packing to true.")
-//	pool.packing = true
-//}
-//
-//func (pool *TxPool) setPackStop() {
-//	pool.packing = false
-//}
+func (pool *TxPool) rangeFunc(key, value interface{}) bool {
+	if len(pool.txQueue) >= maxTxPackageCount {
+		return false
+	}
+	txHash, ok := key.(string)
+	if ok {
+		pool.txQueue <- txHash
+	}
+
+	return true
+}
 
 // Package 用于打包交易，这里返回的是 Transaction 的切片
 // todo： 需要具体观察打包交易时的效率问题
 func (pool *TxPool) Package() []common.Transaction {
 	log.Debugln("Start package transaction...")
-	//pool.setPackStart()
-	//pool.lock.Lock()
-	//log.Infoln("pool locked")
-
-	//defer log.Infoln("pool unlocked")
-	//defer pool.lock.Unlock()
-	//defer pool.packCond.Broadcast()
-	//defer pool.setPackStop()
 
 	count := 0
 	result := make([]common.Transaction, 0, maxTxPackageCount)
 	log.Debugln("Start package tx pool.")
+
+	pool.txs.Range(pool.rangeFunc)
+
 	for idx := 0; idx < maxTxPackageCount; idx++ {
-		//log.Infof("transaction queue length: %d", len(pool.txQueue))
 		if len(pool.txQueue) == 0 || count > maxTxPackageCount {
 			log.Debugln("transaction queue is empty or package finish")
 			break
 		}
 
-		//log.Infof("Package block index %d", idx)
-
 		txHash := <-pool.txQueue
-		metrics.TxPoolMetricsDec()
 		commonHash, err := hex.DecodeString(txHash)
 		if err != nil {
 			log.Errorln("Decode transaction hash failed.")
 			continue
 		}
 
-		tx, err := pool.chain.GetTransactionByHash(common.Hash(commonHash))
-		if tx != nil {
-			pool.txs.Delete(txHash)
-			log.Debugln("Transaction already in database.")
-			continue
-		}
-
 		value, hit := pool.txs.Load(txHash)
-		pool.txs.Delete(txHash)
 		if !hit {
 			continue
 		}
 
+		tx, err := pool.chain.GetTransactionByHash(common.Hash(commonHash))
+		if tx != nil {
+			pool.txs.Delete(txHash)
+			metrics.TxPoolMetricsDec()
+			log.Debugln("Transaction already in database.")
+			continue
+		}
+
+		pool.txs.Delete(txHash)
+		metrics.TxPoolMetricsDec()
 		tx = value.(*common.Transaction)
-		//if !tx.Verify() {
-		//	log.Errorln("Verify failed.")
-		//	continue
-		//}
-		// todo： 这里是传值还是传指针？
+
 		result = append(result, *tx)
 		count++
 	}
@@ -120,22 +101,9 @@ func (pool *TxPool) Package() []common.Transaction {
 }
 
 func (pool *TxPool) Add(transaction *common.Transaction) {
-	//pool.lock.Lock()
-	//defer pool.lock.Unlock()
-
 	txHash := hex.EncodeToString(transaction.Body.Hash[:])
-
-	//for pool.packing {
-	//	pool.packCond.Wait()
-	//}
-
-	select {
-	case pool.txQueue <- txHash:
-		pool.txs.Store(txHash, transaction)
-		metrics.TxPoolMetricsInc()
-	default:
-
-	}
+	pool.txs.Store(txHash, transaction)
+	metrics.TxPoolMetricsInc()
 }
 
 func (pool *TxPool) Contain(hash string) bool {
@@ -144,15 +112,19 @@ func (pool *TxPool) Contain(hash string) bool {
 }
 
 func (pool *TxPool) RemoveTx(hash common.Hash) {
-	//pool.lock.Lock()
-	//defer pool.lock.Unlock()
 	txHash := hex.EncodeToString(hash[:])
 
-	//for pool.packing {
-	//	pool.packCond.Wait()
-	//}
+	_, hit := pool.txs.Load(txHash)
+	if !hit {
+		return
+	}
 
 	pool.txs.Delete(txHash)
+	metrics.TxPoolMetricsDec()
+}
+
+func (pool *TxPool) CleanPool() {
+
 }
 
 func (pool *TxPool) Get(hash string) *common.Transaction {
